ddf5c604d00872bdff844cdfb91f6e3662ac4155
ravibeta <ravibeta@hotmail.com>
2020-10-22 00:11:16 +0000

    add reward api

--- rewardpoints/spring-rewardpoint-rest-rewardpoint/src/main/java/org/springframework/samples/rewardpoint/rewardpoint/RewardPointController.java
+++ rewardpoints/spring-rewardpoint-rest-rewardpoint/src/main/java/org/springframework/samples/rewardpoint/rewardpoint/RewardPointController.java
@@ -15,12 +15,36 @@
  */
 package org.springframework.samples.rewardpoint.rewardpoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.samples.rewardpoint.model.Owner;
+import org.springframework.samples.rewardpoint.model.RewardPoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.inject.Inject;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class RewardPointController {
 
-    /*private final RewardPointRepository rewardpoints;
+    @Value("#{environment['SERVICE_ENDPOINT'] ?: 'localhost:8118'}")
+    private String serviceEndpoint;
+
+    @Inject
+    private final RewardPointRepository rewardpoints;
+    private static final Logger logger = LoggerFactory.getLogger(RewardPointController.class);
 
 
     public RewardPointRepository getRewardPoints() {
@@ -32,10 +56,26 @@ public class RewardPointController {
         this.rewardpoints = rewardpoints;
     }
 
-    // Spring MVC calls method loadRewardPoint(...) before processNewRewardPointForm is called
     @RequestMapping(value = "/owners/{ownerId}/rewardpoints/", method = RequestMethod.POST)
     public void addRewardPoint(RewardPoint rewardpoint) {
         this.rewardpoints.save(rewardpoint);
-    }*/
+    }
 
+    @RequestMapping(value = "/owner/{ownerId}/", method = RequestMethod.GET)
+    public ResponseEntity<List<RewardPoint>> getOwnerRewardPoints(@PathVariable int ownerId){
+        List<RewardPoint> rewardpointList = this.rewardpoints.findByOwnerId(ownerId);
+        if (rewardpointList == null) {
+            return new ResponseEntity<List<RewardPoint>>(rewardpointList, HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<List<RewardPoint>>(rewardpointList, HttpStatus.OK);
+    }
+/*
+curl -i -k 'http://localhost:8118/owner/7/'
+HTTP/1.1 200
+Content-Type: application/json
+Transfer-Encoding: chunked
+Date: Thu, 22 Oct 2020 00:10:38 GMT
+
+[{"id":1,"date":"2013-01-01T00:00:00.000+0000","description":"","ownerId":7,"new":false},{"id":4,"date":"2013-01-04T00:00:00.000+0000","description":"","ownerId":7,"new":false},{"id":20,"date":"2013-01-04T00:00:00.000+0000","description":"","ownerId":7,"new":false}]
+*/
 }
