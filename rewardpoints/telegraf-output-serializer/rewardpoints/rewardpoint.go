6791301563d6fdc6460e7b5879e68652e8aea0ce
ravibeta <ravibeta@hotmail.com>
2020-10-23 14:37:52 +0000

    add reward api

--- rewardpoints/telegraf-output-serializer/rewardpoints/rewardpoint.go
+++ rewardpoints/telegraf-output-serializer/rewardpoints/rewardpoint.go
@@ -1,121 +1,94 @@
-// package rewardpoints
-// 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"fmt"
-// 	"time"
-// 
-// 	"github.com/influxdata/telegraf"
-// )
-// 
-// type serializer struct {
-// 	TimestampUnits time.Duration
-// }
-// 
-// type OIMetric struct {
-//         OwnerId      int64              `json:"owner_id"`
-// 	Description  string             `json:"description"`
-//         Points       string             `json:"points"`
-// 	Date         string             `json:"rewardpoint_date"`
-// }
-// 
-// type OIMetrics []OIMetric
-// 
-// func NewSerializer() (*serializer, error) {
-// 	s := &serializer{}
-// 	return s, nil
-// }
-// 
-// func (s *serializer) Serialize(metric telegraf.Metric) (out []byte, err error) {
-// 	serialized, err := s.createObject(metric)
-// 	if err != nil {
-// 		return []byte{}, nil
-// 	}
-// 	return serialized, err
-// }
-// 
-// func (s *serializer) SerializeBatch(metrics []telegraf.Metric) (out []byte, err error) {
-// 	objects := make([]byte, 0)
-// 	for _, metric := range metrics {
-// 		m, err := s.createObject(metric)
-// 		if err != nil {
-// 			return nil, fmt.Errorf("D! [serializer.rewardpoints] Dropping invalid metric: %s", metric.Name())
-// 		} else if m != nil {
-// 			objects = append(objects, m...)
-// 		}
-// 	}
-// 	replaced := bytes.Replace(objects, []byte("]["), []byte(","), -1)
-// 	return replaced, nil
-// }
-// 
-// func (s *serializer) createObject(metric telegraf.Metric) ([]byte, error) {
-// 	/*  RewardPoints accumulation service supports an array of JSON objects.
-// 	** Following elements accepted in the request body:
-// 		 ** owner_id: 	        The owner of the metric
-// 		 ** description:   	The description of the metric
-// 		 ** points:       	The value of the reward points
-// 		 ** rewardpoint_date:   The date for the grant of reward points in yyyy/MM/dd format
-// 	*/
-// 	var allmetrics OIMetrics
-// 	var oimetric OIMetric
-// 
-// 	oimetric.Source = "Telegraf"
-// 
-// 	// Process Tags to extract node & resource name info
-// 	for _, tag := range metric.TagList() {
-// 		if tag.Key == "" || tag.Value == "" {
-// 			continue
-// 		}
-// 
-// 		if tag.Key == "owner_id" {
-// 			oimetric.OwnerId = tag.Value
-// 		}
-// 
-// 		if tag.Key == "description" {
-// 			oimetric.Description = tag.Value
-// 		}
-// 
-//                 if tag.Key == "points" {
-//                         oimetric.Points = tag.Value
-//                 }
-// 
-//                 if tag.Key == "rewardpoint_date" {
-//                         oimetric.Date = tag.Value
-//                 }
-// 	}
-// 
-// 	// Format timestamp to UNIX epoch
-// 	timestamp := (metric.Time().UnixNano() / int64(time.Millisecond))
-//         oimetric.Date =  fmt.Sprintf("%d/%02d/%02d", timestamp.Year(), timestamp.Month(), timestamp.Day())
-// 
-// 	// Loop of fields value pair and build datapoint for each of them
-// 	for _, field := range metric.FieldList() {
-// 		if !verifyValue(field.Value) {
-// 			// Ignore String
-// 			continue
-// 		}
-// 
-// 		if field.Key == "" {
-// 			// Ignore Empty Key
-// 			continue
-// 		}
-// 
-// 		oimetric.Description = field.Key
-// 		oimetric.Points = field.Value
-// 
-// 		allmetrics = append(allmetrics, oimetric)
-// 	}
-// 
-// 	metricsJson, err := json.Marshal(allmetrics)
-// 
-// 	return metricsJson, err
-// }
-// 
-// func verifyValue(v interface{}) bool {
-// 	switch v.(type) {
-// 	case string:
-// 		return false
-// 	}
-// 	return true
-// }
+package rewardpoints
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/influxdata/telegraf"
+)
+
+type serializer struct {
+	TimestampUnits time.Duration
+}
+
+type OIMetric struct {
+	OwnerId     int    `json:"owner_id"`
+	Description string `json:"description"`
+	Points      int    `json:"points"`
+	Date        string `json:"rewardpoint_date"`
+}
+
+type OIMetrics []OIMetric
+
+func NewSerializer() (*serializer, error) {
+	s := &serializer{}
+	return s, nil
+}
+
+func (s *serializer) Serialize(metric telegraf.Metric) (out []byte, err error) {
+	serialized, err := s.createObject(metric)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return []byte{}, nil
+	}
+	return serialized, err
+}
+
+func (s *serializer) SerializeBatch(metrics []telegraf.Metric) (out []byte, err error) {
+	objects := make([]byte, 0)
+	for _, metric := range metrics {
+		m, err := s.createObject(metric)
+		if err != nil {
+			return nil, fmt.Errorf("D! [serializer.rewardpoints] Dropping invalid metric: %s", metric.Name())
+		} else if m != nil {
+			objects = append(objects, m...)
+		}
+	}
+	replaced := bytes.Replace(objects, []byte("]["), []byte(","), -1)
+	return replaced, nil
+}
+
+func (s *serializer) createObject(metric telegraf.Metric) ([]byte, error) {
+	/*  RewardPoints accumulation service supports an array of JSON objects.
+		** Following elements accepted in the request body:
+			 ** owner_id: 	        The owner of the metric
+			 ** description:   	The description of the metric
+			 ** points:       	The value of the reward points
+			 ** rewardpoint_date:   The date for the grant of reward points in yyyy/MM/dd format
+	           ** Note metric is time-series data and must include timestamp. In this timestamp is already converted to rewardpoint_date
+	*/
+	var allmetrics OIMetrics
+	var oimetric OIMetric
+
+	// Process Tags to extract node & resource name info
+	for _, tag := range metric.TagList() {
+		if tag.Key == "" || tag.Value == "" {
+			continue
+		}
+
+		if tag.Key == "owner_id" {
+			oimetric.OwnerId, _ = strconv.Atoi(tag.Value)
+		}
+
+		if tag.Key == "description" {
+			oimetric.Description = tag.Value
+		}
+
+		if tag.Key == "points" {
+			oimetric.Points, _ = strconv.Atoi(tag.Value)
+		}
+
+		if tag.Key == "rewardpoint_date" {
+			oimetric.Date = tag.Value
+		}
+	}
+	allmetrics = append(allmetrics, oimetric)
+
+	metricsJson, err := json.Marshal(allmetrics)
+
+	return metricsJson, err
+}
+
--- rewardpoints/telegraf-output-serializer/rewardpoints/rewardpoint_test.go
+++ rewardpoints/telegraf-output-serializer/rewardpoints/rewardpoint_test.go
@@ -1,95 +1,80 @@
-// package rewardpoints
-// 
-// import (
-// 	"fmt"
-// 	"sort"
-// 	"testing"
-// 	"time"
-// 
-// 	"github.com/influxdata/telegraf"
-// 	"github.com/influxdata/telegraf/metric"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/require"
-// )
-// 
-// func MustMetric(v telegraf.Metric, err error) telegraf.Metric {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return v
-// }
-// 
-// func TestSerializeMetricFloat(t *testing.T) {
-// 	now := time.Now()
-// 	tags := map[string]string{
-//                 "owner_id": "1",
-// 		"description": "sample",
-//                 "points": 50,
-//                 "rewardpoint_date": "2020/10/22",
-// 	}
-// 	fields := map[string]interface{}{
-//                 "description": "sample",
-//                 "rewardpoint_date": "2020/10/22",
-// 		"points": int64(50),
-//                 "owner_id": int64(1),
-// 	}
-// 	m, err := metric.New("sample", tags, fields, now)
-// 	assert.NoError(t, err)
-// 
-// 	s, _ := NewSerializer()
-// 	var buf []byte
-// 	buf, err = s.Serialize(m)
-// 	assert.NoError(t, err)
-//         timestamp := (now.UnixNano() / int64(time.Millisecond))
-//         date := fmt.Sprintf("%d/%02d/%02d", timestamp.Year(), timestamp.Month(), timestamp.Day())
-// 	expS := []byte(fmt.Sprintf(`[{"owner_id":1,"description":"sample","points":50,"date":"2020/10/22"}]`))
-// 	assert.Equal(t, string(expS), string(buf))
-// }
-// 
-// 
-// func TestSerializeMetricString(t *testing.T) {
-// 	now := time.Now()
-//         tags := map[string]string{
-//                 "owner_id": "1",
-//                 "description": "sample",
-//                 "points": 50,
-//                 "rewardpoint_date": "2020/10/22",
-//         }
-//         fields := map[string]interface{}{
-//                 "description": "sample",
-//                 "rewardpoint_date": "2020/10/22",
-//                 "points": int64(50),
-//                 "owner_id": int64(1),
-//         }
-//         m, err := metric.New("sample", tags, fields, now)
-// 	assert.NoError(t, err)
-// 
-// 	s, _ := NewSerializer()
-// 	var buf []byte
-// 	buf, err = s.Serialize(m)
-// 	assert.NoError(t, err)
-// 
-// 	assert.Equal(t, "null", string(buf))
-// }
-// 
-// func TestSerializeBatch(t *testing.T) {
-// 	m := MustMetric(
-// 		metric.New(
-// 			"sample",
-// 			map[string]string{},
-// 			map[string]interface{}{
-//                             "description": "sample",
-//                             "rewardpoint_date": "2020/10/22",
-//                             "points": int64(50),
-//                             "owner_id": int64(1),
-// 			},
-// 			time.Unix(0, 0),
-// 		),
-// 	)
-// 
-// 	metrics := []telegraf.Metric{m, m}
-// 	s, _ := NewSerializer()
-// 	buf, err := s.SerializeBatch(metrics)
-// 	require.NoError(t, err)
-// 	require.Equal(t, []byte(`[{"owner_id":1,"description":"sample","points":50,"date":"2020/10/22"}]`), buf)
-// }
+package rewardpoints
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/metric"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func MustMetric(v telegraf.Metric, err error) telegraf.Metric {
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func TestSerializeMetricFloat(t *testing.T) {
+	now := time.Now()
+	tags := map[string]string{
+		"owner_id":         "1",
+		"description":      "sample",
+		"points":           "50",
+		"rewardpoint_date": "2020/10/22",
+	}
+	fields := map[string]interface{}{
+		"description":      "sample",
+		"rewardpoint_date": "2020/10/22",
+		"points":           int(50),
+		"owner_id":         int(1),
+	}
+	m, err := metric.New("sample", tags, fields, now)
+	assert.NoError(t, err)
+
+	s, _ := NewSerializer()
+	var buf []byte
+	buf, err = s.Serialize(m)
+	assert.NoError(t, err)
+	timestamp := (now.UnixNano() / int64(time.Millisecond))
+	i, err := strconv.ParseInt(fmt.Sprint(timestamp), 10, 64)
+	if err == nil {
+		tm := time.Unix(i, 0)
+		date := fmt.Sprintf("%d/%02d/%02d", tm.Year(), tm.Month(), tm.Day())
+		fmt.Printf("%s", date)
+	}
+	expS := []byte(fmt.Sprintf(`[{"owner_id":1,"description":"sample","points":50,"rewardpoint_date":"2020/10/22"}]`))
+	assert.Equal(t, string(expS), string(buf))
+}
+
+func TestSerializeBatch(t *testing.T) {
+	m := MustMetric(
+		metric.New(
+			"sample",
+			map[string]string{
+				"owner_id":         "1",
+				"description":      "sample",
+				"points":           "100",
+				"rewardpoint_date": "2020/10/22",
+			},
+			map[string]interface{}{
+				"description":      "sample",
+				"rewardpoint_date": "2020/10/22",
+				"points":           int(50),
+				"owner_id":         int(1),
+			},
+			time.Unix(0, 0),
+		),
+	)
+
+	metrics := []telegraf.Metric{m, m}
+	s, _ := NewSerializer()
+	buf, err := s.SerializeBatch(metrics)
+	require.NoError(t, err)
+	fmt.Print("buf+" + string(buf))
+	require.Equal(t, []byte(`[{"owner_id":1,"description":"sample","points":100,"rewardpoint_date":"2020/10/22"},{"owner_id":1,"description":"sample","points":100,"rewardpoint_date":"2020/10/22"}]`), buf)
+}
