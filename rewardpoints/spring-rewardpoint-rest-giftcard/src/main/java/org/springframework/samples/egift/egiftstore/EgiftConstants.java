2fdc2fdbf5c4fc410fadbb9ced96a769724abb27
ravibeta <ravibeta@hotmail.com>
2020-10-31 22:09:59 +0000

    add reward api

---
+++ rewardpoints/spring-rewardpoint-rest-giftcard/src/main/java/org/springframework/samples/egift/egiftstore/EgiftConstants.java
@@ -0,0 +1,10 @@
+package org.springframework.samples.egift.egiftstore;
+
+public class EgiftConstants {
+    protected static final String DEFAULT_HTTPSURL = "https://rewards-api.egifter.com";
+    protected static final String DEFAULT_METHOD = "/v1/Token";
+    protected static final String REGISTERED_EMAIL = "admin@rewardpointsb2b.com";
+    protected static final String REGISTERED_PUBLISHABLE = "pk_test_51HVmXmCUt3t8PcFNQX0uBift2rFvuRe7BCqQiaO8vbdLXJSXz0AQk93p4CrqborjZQOc0DIPDdqw307Ka0FgrSv300eegZhJU8";
+    protected static final String REGISTERED_SECRET = "sk_test_51HVmXmCUt3t8PcFNUT4BXnvVzSCR0sVRz0e4cbZHBTGRt4V2a6t6HGj7YyhoudaYkGX1CGkJDPJhZc6StABwBj6L00WFUdr8n2";
+    protected static final String REGISTERED_ACCOUNT = "acct_1HVmXmCUt3t8PcFN";
+}
--- rewardpoints/spring-rewardpoint-rest-giftcard/src/main/java/org/springframework/samples/egift/egiftstore/EgiftController.java
+++ rewardpoints/spring-rewardpoint-rest-giftcard/src/main/java/org/springframework/samples/egift/egiftstore/EgiftController.java
@@ -25,15 +25,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpClientErrorException;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ReadContext;
 
+import com.google.gson.Gson;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.inject.Inject;
+import javax.net.ssl.HttpsURLConnection;
 import java.io.StringReader;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Base64;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 
 @RestController
@@ -45,6 +71,12 @@ public class EgiftController {
     @Inject
     private final EgiftRepository egifts;
     private static final Logger logger = LoggerFactory.getLogger(EgiftController.class);
+    private static String apiPath = EgiftConstants.DEFAULT_METHOD;
+    private static String httpsURL = EgiftConstants.DEFAULT_HTTPSURL;
+    private static String email = EgiftConstants.REGISTERED_EMAIL;
+    private static String clientId = EgiftConstants.REGISTERED_PUBLISHABLE;
+    private static String clientSecret = EgiftConstants.REGISTERED_SECRET + ":";
+    private static String accountId = EgiftConstants.REGISTERED_ACCOUNT;
 
 
     public EgiftRepository getEgifts() {
@@ -72,4 +104,58 @@ public class EgiftController {
         }
         return new ResponseEntity<List<Egift>>(egiftList, HttpStatus.OK);
     }
+
+    public static void main(String[] args) throws Exception {
+    URL myurl = new URL(httpsURL+apiPath);
+    HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Content-Type","application/x-www- form-urlencoded"); 
+    con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0;Windows98;DigExt)"); 
+    con.setRequestProperty("Authorization", "Basic " + Base64.getEncoder().encodeToString(new String(clientSecret+":").getBytes()));
+    con.setRequestProperty("Stripe-Account", accountId);
+    con.setDoInput(true); 
+    DataInputStream input = new DataInputStream( con.getInputStream() ); 
+    for( int c = input.read(); c != -1; c = input.read() ) 
+    System.out.print( (char)c ); 
+    input.close(); 
+
+    System.out.println("Resp Code:"+con .getResponseCode()); 
+    System.out.println("Resp Message:"+ con .getResponseMessage()); 
+   }
+
+   private String getToken() {
+    String url = httpsURL + apiPath;
+    HttpHeaders requestHeaders=new HttpHeaders();
+    Base64.Encoder encoder = Base64.getEncoder();
+    String encodedEmail = encoder.encodeToString(email.getBytes(StandardCharsets.UTF_8));
+    requestHeaders.set("Content-Type","application/json");
+    requestHeaders.set("Email", encodedEmail);
+    requestHeaders.set("AccessToken", clientSecret);
+    RestTemplate rest = new RestTemplate();
+    String text = rest.postForObject(url,
+            new HttpEntity("", requestHeaders), String.class);
+    ReadContext response = JsonPath.parse(text);
+    try(JsonReader jsonReader = Json.createReader(new StringReader(response.toString()))) {
+         JsonObject result = jsonReader.readObject();
+         String token = result.getString("value");
+         return token;
+    }
+   }
+
+    public String createGiftCard(@RequestParam(value="email", defaultValue="noone@none.com") String email) {
+    String tokentext  = getToken();
+    String url = httpsURL+ "/v1/Orders";
+    String params= "{\"type\": \"Codes\", \"lineItems\":[{\"id1\"}], \"poNumber\": \"PO-123456789\"}";
+    HttpHeaders requestHeaders=new HttpHeaders();
+    requestHeaders.set("Content-Type","application/json");
+    requestHeaders.set("Authorization","Bearer " + tokentext);
+    RestTemplate rest = new RestTemplate();
+    ReadContext response = JsonPath.parse(rest.postForObject(url,
+            new HttpEntity(params, requestHeaders), String.class));
+    try(JsonReader jsonReader = Json.createReader(new StringReader(response.toString()))) {
+         JsonObject result = jsonReader.readObject();
+         String id = result.getString("id");
+         return id;
+    }
+   }
 }
