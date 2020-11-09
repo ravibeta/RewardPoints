3689c05f3696ccd0dc8e1bd570d624aaa06c1748
Ravi Rajamani <ravibeta@hotmail.com>
2020-11-08 16:39:43 -0800

    add PubSub

---
+++ rewardpoints/PubSub/src/test/java/com/rewardpoints/event/NotificationSystemTest.java
@@ -0,0 +1,19 @@
+import com.rewardpoints.event;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class NotificationSystemTest {
+    @Test
+    public void testCreation() {
+        System.err.println("Hello world!");
+        Assert.assertEquals(false, true);
+        NotificationSystem notificationSystem = new NotificationSystem();
+        RewardPointNotifier notifier = new RewardPointNotifier(notificationSystem);
+        RewardPointSubscriber listener = new RewardPointNotifier(notificationSystem);
+        notifier.subscribe("Notification", listener);
+        listener.subscribe("Notification", notifier);
+        Assert.assertEquals(notificationSystem.isNotifierPresent(), true);
+        Assert.assertEquals(notificationSystem.isListenerPresent(), true);
+    }
+}
